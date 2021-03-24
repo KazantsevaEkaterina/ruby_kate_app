d6387e42b3537e99a572870efb0f0c8e2abc24eb
Ekaterina12345 <iiiii21111998@gmail.com>
2021-03-24 23:37:07 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :users
+  get 'main/index'
+  get 'main/help'
+  get 'main/contacts'
+  get 'main/about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
