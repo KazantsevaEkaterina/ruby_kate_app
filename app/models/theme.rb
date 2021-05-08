4795070f04ef7161a53bddc237916903dbf71d51
Ekaterina12345 <iiiii21111998@gmail.com>
2021-05-08 22:48:42 +0300

    6 lab

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,7 +1,5 @@
 class Theme < ApplicationRecord
   has_many :images
 
-  scope :find_theme_id, -> (name) {
-    select('id').where(name: name)
-  }
+  scope :find_theme_id, -> (theme) { find_by_name(theme).id }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,35 @@
 Rails.application.routes.draw do
-  root 'work#index'
-
-  get 'work/index'
-  get 'work/choose_theme'
-  get 'work/display_theme'
+  resources :users
+  resources :values
   resources :themes
   resources :images
-  resources :values
-  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  namespace :api, defaults: { format: :json } do
+    match 'next_image',       to: 'api#next_image',   via: :get
+    match 'prev_image',       to: 'api#prev_image',   via: :get
+    match 'save_value',       to: 'api#save_value',   via: :get
+  end
+
   get 'main/index'
   get 'main/help'
   get 'main/contacts'
   get 'main/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #
-  # work
 
-  match 'work', to: 'work#index', via: 'get'
-  match 'choose_image', to: 'work#choose_image', via: :get
+  match 'work', to: 'work#index',via: 'get'
+  match 'choose_image',     to: 'work#choose_image',      via: :get
   match 'choose_theme', to: 'work#choose_theme', via: :get
-  match 'display_theme', to: 'work#display_theme', via: :post
+  match 'display_theme', to: 'work#display_theme', via: :post, as: :js
 
-  #root 'main#index'
+  match 'results_list',     to: 'work#results_list', via: :get
+
+  match 'save_value',       to: 'api/api#save_value',   via: :get
+
+  match 'signup',   to: 'users#new',            via: 'get'
+  match 'signin',   to: 'sessions#new',         via: 'get'
+  match 'signout',  to: 'sessions#destroy',     via: 'delete'
+
+  root 'work#index'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
